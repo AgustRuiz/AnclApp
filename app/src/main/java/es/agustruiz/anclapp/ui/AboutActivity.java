440bec763b193d152d33cafc1ee80a18c7aa7c19
Agustín Ruiz Linares <agustinruizlinares@gmail.com>
2016-05-18 11:26:15 +0200

    [ADD] About activity

--- app/src/main/java/es/agustruiz/anclapp/ui/AboutActivity.java
+++ app/src/main/java/es/agustruiz/anclapp/ui/AboutActivity.java
@@ -1,15 +1,42 @@
 package es.agustruiz.anclapp.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import es.agustruiz.anclapp.R;
 
 public class AboutActivity extends AppCompatActivity {
+
+    @Bind(R.id.tvDeveloperEmail)
+    TextView developerEmail;
+
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        mContext = getApplicationContext();
+        ButterKnife.bind(this);
+        initialize();
+    }
+
+    private void initialize(){
+        developerEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{getString(R.string.about_developer_email)});
+                intent.putExtra(Intent.EXTRA_SUBJECT, String.format("[%s] ", getString(R.string.app_name)));
+                startActivity(Intent.createChooser(intent, ""));
+            }
+        });
     }
 }
